8c63264a662d1ef2cf585d5376d3e35b76181a9c
AndresXD <andrespro529@gmail.com>
2020-12-12 04:03:33 -0300

    extra envio a login

--- resources/js/pages/LoginComponent.vue
+++ resources/js/pages/LoginComponent.vue
@@ -39,20 +39,20 @@
                                     </a>
                                 </div>
                                 <div class="p-2">
-                                    <form class="form-horizontal" action="index.html">
+                                    <form method="POST" class="form-horizontal" action="auth/login">
 
                                         <div class="form-group">
                                             <label for="username">Correo electronico</label>
-                                            <input type="text" class="form-control" id="username" placeholder="Ingrese su correo">
+                                            <input type="text" class="form-control" id="email" v-model="todo.email" placeholder="Ingrese su correo">
                                         </div>
 
                                         <div class="form-group">
                                             <label for="userpassword">Contraseña</label>
-                                            <input type="password" class="form-control" id="userpassword" placeholder="Ingrese su contraseña">
+                                            <input type="password" class="form-control" id="password" todo.password placeholder="Ingrese su contraseña">
                                         </div>
 
                                         <div class="mt-3">
-                                            <button class="btn btn-primary btn-block waves-effect waves-light" type="submit">Ingresar</button>
+                                            <button @click="enviar()" class="btn btn-primary btn-block waves-effect waves-light" type="submit">Ingresar</button>
                                         </div>
  <div class="mt-5 text-center">
 
@@ -76,7 +76,12 @@
 <script>
 export default {
     data() {
+
     return {
+         todo:{
+            email:"",
+            password:""
+        },
         video1:"https://player.vimeo.com/external/451754254.sd.mp4?s=d91618dfbf25e5b5ebedcc5b6c23d33ac5f5478e&profile_id=139&oauth2_token_id=57447761",
         video2:"https://player.vimeo.com/external/451834055.sd.mp4?s=b395f49a0ac32ec44af2ef2ad307b7149b26ae89&profile_id=139&oauth2_token_id=57447761",
         video3:"https://player.vimeo.com/external/371853906.sd.mp4?s=fbde7b607df84867c1dc8741fc74b9d6433cef9f&profile_id=139&oauth2_token_id=57447761",
@@ -84,6 +89,13 @@ export default {
 },
 
 methods: {
+    enviar(){
+         axios.post("auth/login",{dato:todo}).then((res) => {
+      });
+      this.todo.email ="";
+      this.todo.password="";
+
+    },
     loadVideos(){
         var video1 = document.getElementById('video1');
         var video2 = document.getElementById('video2');
